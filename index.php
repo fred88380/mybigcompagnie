ab04c0ea0928ffd30a343d782c64b0189ad5ac72
Frédéric Semol <126673398+fred88380@users.noreply.github.com>
2025-01-24 10:29:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -205,5 +205,4 @@ $getEmployees = getAllEmployees($db);
     <script src="./js/modal.js"></script>
     <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
-</html>monica
+</html>
